f51b5e4a4d6edf4c110e429e0bea8691334888de
James Halliday <mail@substack.net>
2011-08-19 21:29:16 -0700

    res.headers, seems to work in all the browsers

--- example/get/main.js
+++ example/get/main.js
@@ -3,8 +3,6 @@ var http = require('http');
 http.get({ path : '/beep' }, function (res) {
     var div = document.getElementById('result');
     
-    div.innerHTML += JSON.stringify(res.headers) + '<br>';
-    
     res.on('data', function (buf) {
         div.innerHTML += buf;
     });
--- example/streaming/main.js
+++ example/streaming/main.js
@@ -2,7 +2,12 @@ var http = require('http');
 
 http.get({ path : '/doom' }, function (res) {
     var div = document.getElementById('result');
+    
+    for (var key in res.headers) {
+        div.innerHTML += key + ' => ' + res.getHeader(key) + '<br>';
+    }
     div.innerHTML += res.statusCode + ': ';
+    
     if (!div.style) div.style = {};
     
     div.style.color = 'rgb(80,80,80)';
