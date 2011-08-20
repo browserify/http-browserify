99fefaa5cc5b58587ba46d5ec7549997431d8f1c
James Halliday <mail@substack.net>
2011-08-19 22:03:35 -0700

    strip header stuff out of the streaming example

--- example/streaming/main.js
+++ example/streaming/main.js
@@ -2,14 +2,7 @@ var http = require('http');
 
 http.get({ path : '/doom' }, function (res) {
     var div = document.getElementById('result');
-    
-    for (var key in res.headers) {
-        div.innerHTML += key + ' => ' + res.getHeader(key) + '<br>';
-    }
-    div.innerHTML += res.statusCode + ': ';
-    
     if (!div.style) div.style = {};
-    
     div.style.color = 'rgb(80,80,80)';
     
     res.on('data', function (buf) {
