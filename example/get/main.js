abbf4337a37b0cd4a59de0e586012bda226a3a0b
James Halliday <mail@substack.net>
2011-08-19 21:37:48 -0700

    simple get example

--- example/get/main.js
+++ example/get/main.js
@@ -2,12 +2,13 @@ var http = require('http');
 
 http.get({ path : '/beep' }, function (res) {
     var div = document.getElementById('result');
+    div.innerHTML += 'GET /beep<br>';
     
     res.on('data', function (buf) {
         div.innerHTML += buf;
     });
     
     res.on('end', function () {
-        div.innerHTML += '!';
+        div.innerHTML += '<br>__END__';
     });
 });
