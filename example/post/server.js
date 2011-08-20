cdb6192caf7f888768537e13983ee817c9efa1cc
James Halliday <mail@substack.net>
2011-08-19 21:51:17 -0700

    post requests, example works

---
+++ example/post/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express.createServer();
+app.use(express.static(__dirname));
+
+app.post('/plusone', function (req, res) {
+    res.setHeader('content-type', 'text/plain');
+    
+    var s = '';
+    req.on('data', function (buf) { s += buf.toString() });
+    
+    req.on('end', function () {
+        var n = parseInt(s) + 1;
+        res.end(n.toString());
+    });
+});
+
+var browserify = require('browserify');
+var bundle = browserify({
+    entry : __dirname + '/main.js',
+    require : { http : 'http-browserify' },
+    watch : true
+});
+app.use(bundle);
+
+console.log('Listening on :8082');
+app.listen(8082);
