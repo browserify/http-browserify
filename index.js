5ab049277c238b918f8a0452eac8bac5ceea830e
James Halliday <mail@substack.net>
2013-11-10 16:03:26 +0800

    only take the window.location.port when params.host isn't passed in

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var Request = require('./lib/request');
 
 http.request = function (params, cb) {
     if (!params) params = {};
-    if (!params.host) params.host = window.location.host.split(':')[0];
-    if (!params.port) params.port = window.location.port;
+    if (!params.host && !params.port) {
+        params.port = parseInt(window.location.port, 10);
+    }
+    if (!params.host) params.host = window.location.hostname;
+    if (!params.port) params.port = 80;
     if (!params.scheme) params.scheme = window.location.protocol.split(':')[0];
     
     var req = new Request(new xhrHttp, params);
